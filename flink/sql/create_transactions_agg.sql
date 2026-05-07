70fe887544fd92e2e9a25ff80873491dab931b2f
Sidharth Panda <sidharth.panda@myyahoo.com>
2026-05-07 18:54:38 +0530

    Fixed flink sql

--- flink/sql/create_transactions_agg.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE transactions_agg (
-    window_start TIMESTAMP(3),
-    window_end TIMESTAMP(3),
-    account_id STRING,
-    total_amount DOUBLE,
-    txn_count BIGINT
-) WITH (
-    'connector' = 'print'
-);
